18928971adfc6169c04e5754e5d43dbf2ed3f70d
unknown <thebeangodzilla22@gmail.com>
2023-02-22 19:55:40 +0800

    2/22/2023 Commit

--- Blackjack Mk.1/blackjackscript.js
+++ Blackjack Mk.1/blackjackscript.js
@@ -1,11 +1,28 @@
 var playerName
+var betAmount
 const playerCardsDiv = document.getElementById("playerCardElement")
-const openDialogue = document.getElementById("opDia")
+const openDialogue = document.getElementById("nameDia")
 const gameWindow = document.getElementById("gameWindowElement")
 const playBtn = document.getElementById("play")
 const standBtn = document.getElementById("standBtnElement")
 const hitBtn = document.getElementById("hitBtnElement")
 const sumLabel = document.getElementById("playerSumElement")
+const dealerSumLabel = document.getElementById("dealerSumElement")
+const betBtn = document.getElementById("setAmount")
+const betDialogue = document.getElementById("betWindowElement")
+const playerMovesDiv = document.getElementById("playerMoveDiv")
+const playerNameDiv = document.getElementById("playerNameDivElement")
+const betInput = document.getElementById("betAmountTxtBox")
+const dealerCardsDiv = document.getElementById("dealerCardElement")
+var playerSum
+var playerCards
+var playerCash = 5000
+var dealerCards
+var dealerSum
+const retryButton = document.createElement("button")
+const winOrLose = document.createElement("h3")
+retryButton.innerHTML = "Retry"
+retryButton.addEventListener("click", retry)
 
 class Player {
     constructor(playerName, playerMoney, playerCards) {
@@ -55,57 +72,107 @@ class Cards {
     }
 }
 
-var playerCards = [new Cards(Math.floor(Math.random() * (12 - 2) + 2)), new Cards(Math.floor(Math.random() * (12 - 2) + 2))]
+function initPlayerCards() {
+    playerCards = [new Cards(Math.floor(Math.random() * (12 - 2) + 2)), new Cards(Math.floor(Math.random() * (12 - 2) + 2))]
+}
+
+function initDealerCards() {
+    dealerCards = [new Cards(Math.floor(Math.random() * (12 - 2) + 2)), new Cards(Math.floor(Math.random() * (12 - 2) + 2))]
+}
 
 playBtn.addEventListener("click", function(){
-    if (openDialogue.className==="dialogueDiv") {
+    if (openDialogue.className==="openDialogue") {
         openDialogue.className += " clicked"
-        gameWindow.className += " active"
+        betDialogue.className += " betNotSet"
         playerName = document.getElementById("nameTxtBox").value
-        document.getElementById("playerNameElement").innerHTML = `${playerName}:`
-        renderCards(playerCards)
     } else {
-        openDialogue.className = "dialogueDiv"
-        gameWindow.className += "gameWindow"
+        openDialogue.className = "openDialogue"
+        gameWindow.className = "gameWindow"
         console.log("reverting")
     }
 })
 
+betBtn.addEventListener("click", function(){
+    if (betDialogue.className==="betDialogue betNotSet" && betInput.value.match("^[0-9]*$")) {
+        betDialogue.className = "betDialogue betSet"
+        gameWindow.className += " active"
+        betAmount = betInput.value
+        document.getElementById("playerNameElement").innerHTML = `${playerName}: ${playerCash}`
+        document.getElementById("playerBetAmount").innerHTML = `Bet: ${betAmount}`
+        initPlayerCards()
+        initDealerCards()
+        renderCards(playerCardsDiv, playerCards)
+        renderCards(dealerCardsDiv, dealerCards)
+    } else {
+        betDialogue.className = "betDialogue betNotSet"
+        gameWindow.className = "gameWindow" 
+        alert(`'Please enter a valid bet amount'`)
+    }
+ })
 
 function hitMove() {
     let newCard = new Cards(Math.floor(Math.random() * (12 - 2) + 2))
     playerCards.push(newCard)
     playerCardsDiv.innerHTML += newCard.cardHtml()
-    renderSum(playerCardElement)
+    renderPlayerSum()
+    renderGame()
 }
 
 function standMove() {
-
-}
-
-function renderCards(playerCards) {
-    for (let i = 0; i < playerCards.length; i++) {
-        playerCardsDiv.innerHTML += playerCards[i].cardHtml()
-    }
-    renderSum(playerCardElement)
-}
-
-function renderSum(playerCardElement) {
-    let sum = 0
-    for (let i = 0;  i < playerCards.length; i++) {
-        sum += Cards.cardVal(playerCards[i])
-    }
-    sumLabel.innerHTML = `Sum: ${sum}`
-}
-
-
-function renderGame() { 
+    let newCard = new Cards(Math.floor(Math.random() * (12 - 2) + 2))
     
 }
 
-function loseEvent() {
-
+function renderCards(cardsDiv, cards) {
+    cardsDiv.innerHTML = ""
+    for (let i = 0; i < playerCards.length; i++) {
+        cardsDiv.innerHTML += cards[i].cardHtml()
+    }
+    renderPlayerSum()
+    renderDealerSum()
 }
+
+function renderPlayerSum() {
+    playerSum = 0
+    for (let i = 0;  i < playerCards.length; i++) {
+        playerSum += Cards.cardVal(playerCards[i])
+    }
+    sumLabel.innerHTML = `Sum: ${playerSum}`
+}
+
+function renderDealerSum() {
+    dealerSum = 0
+    for (let i = 0; i < dealerCards.length; i++) {
+        dealerSum += Cards.cardVal(dealerCards[i])
+    }
+    dealerSumLabel.innerHTML = `Sum: ${dealerSum}`
+}
+
+function renderGame() { 
+    if (playerSum > 21) {
+        loseEvent()
+    } 
+}
+
+function loseEvent() {
+    winOrLose.innerHTML = "You lose"
+    playerCash -= betAmount
+    playerMovesDiv.appendChild(retryButton)
+    playerNameDiv.appendChild(winOrLose)
+    hitBtn.disabled = true
+    standBtn.disabled = true
+}
+
+ function retry() {
+    betDialogue.className = "betDialogue betNotSet"
+    gameWindow.className = "gameWindow"
+    playerCards = []
+    playerMovesDiv.removeChild(retryButton)
+    playerNameDiv.removeChild(winOrLose)
+    hitBtn.disabled = false
+    standBtn.disabled = false
+ }
+
 
 function winEvent() {
 
